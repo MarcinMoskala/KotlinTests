08f5f6558f97509eb03bdfcc1d781b41ed6bd147
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-30 12:21:40 +0200

    QuickSort change from if to when

--- src/sweet/QuickSort.kt
+++ src/sweet/QuickSort.kt
@@ -3,13 +3,14 @@ package sweet
 import measureTime
 import java.util.*
 
-fun <T : Comparable<T>> List<T>.quickSort(): List<T> =
-        if (size < 2) this
-        else {
-            val pivot = first()
-            val (smaller, greater) = subList(1, size).partition { it <= pivot }
-            smaller.quickSort() + pivot + greater.quickSort()
-        }
+fun <T : Comparable<T>> List<T>.quickSort(): List<T> = when {
+    size < 2 -> this
+    else -> {
+        val pivot = first()
+        val (smaller, greater) = subList(1, size).partition { it <= pivot }
+        smaller.quickSort() + pivot + greater.quickSort()
+    }
+}
 
 fun main(args: Array<String>) {
     listOf(10, 100, 1000, 10000, 100000, 1000000)
@@ -23,7 +24,8 @@ fun main(args: Array<String>) {
 
 inline fun <T> suppose(f: () -> T) = f()
 
-fun kaka(c: Int): Int = suppose {
+fun kaka(c: Int)
+        : Int = suppose {
     val a = 1
     val b = 20
     a + b + c
