0486b4ca0766c651434aeff370dd392d8e20edf1
marcinmoskala <marcinmoskala@gmail.com>
2017-05-07 12:25:29 +0200

    Pinp-up QuickSort

--- src/sweet/QuickSort.kt
+++ src/sweet/QuickSort.kt
@@ -7,7 +7,7 @@ fun <T : Comparable<T>> List<T>.quickSort(): List<T> = when {
     size < 2 -> this
     else -> {
         val pivot = first()
-        val (smaller, greater) = subList(1, size).partition { it <= pivot }
+        val (smaller, greater) = drop(1).partition { it <= pivot }
         smaller.quickSort() + pivot + greater.quickSort()
     }
 }
